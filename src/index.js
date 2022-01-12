1782edc9e579ea52d56cf809b68fd9ff2e71a4b1
Seborider <mayer.sebobo@gmail.com>
2022-01-12 17:54:14 +0100

    🐛  Fix import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BudgetsProvider } from './contexts/BudgetContexts';
+import { BudgetsProvider } from './contexts/BudgetsContexts';
 
 ReactDOM.render(
   <React.StrictMode>
