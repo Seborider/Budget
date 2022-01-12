f7ef3d833798a1af7537d3ca6332601eb653ec44
Seborider <mayer.sebobo@gmail.com>
2022-01-12 11:19:59 +0100

    ✨  Add header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import Container from 'react-bootstrap/Container'
+import {Stack, Button} from 'react-bootstrap'
+
 function App() {
   return (
- " dasdd"
+<Container className="my-4">
+  <Stack direction="horizontal" gap="2" className="mb-4">
+    <h1 className="me-auto">Budgets</h1>
+    <Button variant="primary">Add Budget</Button>
+    <Button variant="outline-primary">Add Expense</Button>
+  </Stack>
+  <div style={{display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", alignItems: "flex-start" }}></div>
+</Container>
   );
 }
 
